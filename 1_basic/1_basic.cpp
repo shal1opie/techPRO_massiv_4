17d95c00f6c58b9f65eacf8847b6431bdc08f0dc
Алиев Расим Рауфович <1arr@spbt.local>
2023-05-27 11:56:19 +0300

    proga

---
+++ 1_basic/1_basic.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <stdlib.h> 
+#include <time.h> 
+
+using namespace std;
+
+int first() {
+	srand(time(0));
+	int i;
+	int n = 0;
+	const int m = 14;
+	int a[m];
+	for (int i = 0; i < 14; i++) {
+		a[i] = rand() % 100;
+		cout << a[i] << " ";
+		if (a[i] % 2 == 0) {
+			n++;
+		}
+	}
+	cout << n;
+	return n;
+}
+
+
+int main()
+{
+	first();
+
+}
